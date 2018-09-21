d0871c47cc2c68a24a6d7d67264d6a601acf2742
ameerthehacker <ameerjhanprof@gmail.com>
2018-09-21 12:16:14 +0530

    Initial commit

---
+++ TaxCalculator.Cross/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TaxCalculator.Cross
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            int amount;
+            Int32.TryParse(txtAmount.Text, out amount);
+            double result = amount * 0.3;
+
+            lblResult.Text = result.ToString();
+        }
+    }
+}
